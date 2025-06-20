c036450d8dcfeafd5a88842e2f567cee234432c0
danimefalls <kondakov.d,study@gmail.com>
2025-06-20 22:19:10 +0300

    3. Добавление переменых для элементов.

--- app/src/main/java/com/example/string/MainActivity.kt
+++ app/src/main/java/com/example/string/MainActivity.kt
@@ -1,20 +1,14 @@
 package com.example.string
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
+
+    private lateinit var inputText: EditText
+    private lateinit var countButton: Button
+    private lateinit var resultText: TextView
 }
